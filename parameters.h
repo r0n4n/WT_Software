4b23eb4e42dbb79104f77df2b719fe40c93a15b9
r0n4n <ronan.blanchard35@gmail.com>
2018-05-03 18:24:12 +0300

    add parameters files

---
+++ parameters.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   parameters.h
+ * Author: rblan
+ *
+ * Created on 3 mai 2018, 17:54
+ */
+
+#ifndef PARAMETERS_H
+#define	PARAMETERS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+#define L 0.0001f // line inductance 
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PARAMETERS_H */
+
