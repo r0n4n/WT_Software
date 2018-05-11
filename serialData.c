b29ad4c332fa35a78384f0d6009434c078e074e0
r0n4n <ronan.blanchard35@gmail.com>
2018-05-11 13:06:37 +0300

    add SerialVect function to easily send a vect of data using the '\n' terminator block

--- serialData.c
+++ serialData.c
@@ -8,6 +8,24 @@ union u
     uint8_t s[4]; /**< acesso a pedaço de mémória de 32 bits pedaços correspondentes a caractéres. */
 };
 
+ union u2
+{
+    int i; /**< acesso a pedaço de mémória de 32 bits através de tipo inteiro sem sinal. */
+    float f; 
+    char s[4]; /**< acesso a pedaço de mémória de 32 bits pedaços correspondentes a caractéres. */
+};
+
+
+ union u2 chariot ;
+ 
+ 
+
+void serialInit() {
+    chariot.s[0] = '\n' ; 
+    chariot.s[1] = 0 ; 
+    chariot.s[2] = 0 ; 
+    chariot.s[3] = 0 ;
+}
 
 void sendData(float data){
       union u number ;
@@ -21,4 +39,13 @@ void sendData(float data){
 
 void ReceiveData(float data){
     
+}
+
+void sendVect(float *vect, int length ){
+    int i ; 
+    
+    for(i=0;i<length;i++){
+        sendData(vect[i]) ; 
+    } 
+    sendData(chariot.f) ; 
 }
