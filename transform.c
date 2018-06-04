14f52a3b34285017ec362b1f885ee9c09503666d
r0n4n <ronan.blanchard35@gmail.com>
2018-06-04 16:36:52 +0200

    can perform the transformations without overload

--- transform.c
+++ transform.c
@@ -4,9 +4,13 @@
 
 #define ONE_BY_THREE 333 // 1/3 *1000  
 #define ONE_BY_SQRT_THREE 577 // 1/srqt(3) * 1000
-#define ONE_BY_SQRT_SIX 408 // 1/srqt(6) * 1000
-#define ONE_BY_SQRT_TWO 707 // 1/srqt(2) * 1000
-#define SQRT_TWO_BY_TRHEE 816 // sqrt(2/3) * 1000 
+#define ONE_BY_SQRT_SIX 40 // 1/sqrt(6) = 0.4082
+#define ONE_BY_SQRT_TWO 70 // 1/sqrt(2)=  0.7071
+#define SQRT_TWO_BY_TRHEE 81 // sqrt(2/3) = 0.8165 
+#define SQRT_SIX 24  // 2.4495
+#define SQRT_TWO 14 // 1.4142
+#define ONE_BY_SQRT_TWO_BY_TRHEE 1224 //1.2247
+ 
 
 /* Formula taken from https://en.wikipedia.org/wiki/Direct-quadrature-zero_transformation */
 
@@ -18,10 +22,10 @@
 alphabeta abc_to_alphabeta(abc abc) {
     alphabeta alphabeta;
     
-    alphabeta.alpha = (2*abc.a - abc.b - abc.c)*ONE_BY_SQRT_SIX ;
-    alphabeta.beta = (abc.b - abc.c)*ONE_BY_SQRT_TWO;
-    alphabeta.alpha /= 1000 ;
-    alphabeta.beta /=1000 ;
+    alphabeta.alpha = (2*abc.a - abc.b - abc.c)/SQRT_SIX ;
+    alphabeta.beta = (abc.b - abc.c)/SQRT_TWO;
+//    alphabeta.alpha /= 1000 ;
+//    alphabeta.beta /=1000 ;
     return alphabeta; // THE VALUES ARE *1 000 
 }
 
@@ -30,14 +34,14 @@ dq alphabeta_to_dq(alphabeta alphabeta, float theta){
     dq dq;
 //    printf("theta=%f   \n\r", theta); 
 //    printf("theta=%f   \n\r", theta); 
-    int cos_theta = (int)cosf(theta)*1000 ; 
-    int sin_theta = (int)sinf(theta)*1000 ; 
+    int cos_theta = (int)(cosf(theta)*1000) ; 
+    int sin_theta = (int)(sinf(theta)*1000) ; 
 //    printf("cos theta=%ld   ", cos_theta);
 //    printf("sin_theta=%ld   \n\r", sin_theta);
     dq.d = cos_theta*alphabeta.alpha + sin_theta*alphabeta.beta;
     dq.q = cos_theta*alphabeta.beta - sin_theta*alphabeta.alpha;
-    dq.d/=1000 ; 
-    dq.q/=1000 ; 
+//    dq.d/=100 ; 
+//    dq.q/=100 ; 
 //    printf("d=%ld   ",dq.d );
 //    printf("q=%ld   \n\r",dq.q );
     return dq;
@@ -61,9 +65,10 @@ dq alphabeta_to_dq2(alphabeta alphabeta,  trigo_type cos_theta , trigo_type sin_
 
 alphabeta dq_to_alphabeta(dq dq, float theta){
     alphabeta alphabeta;
-    
-    int cos_theta = (int)(cosf(theta)*1000) ; 
-    int sin_theta = (int)(sinf(theta)*1000) ; 
+    dq.d /= 100 ; 
+    dq.q /= 100 ;
+    int cos_theta = (int)(cosf(theta)*100) ; 
+    int sin_theta = (int)(sinf(theta)*100) ; 
     
     alphabeta.alpha = cos_theta*dq.d - sin_theta*dq.q;
     alphabeta.beta = sin_theta*dq.d + cos_theta*dq.q;
@@ -80,13 +85,17 @@ alphabeta dq_to_alphabeta2(dq dq, trigo_type cos_theta , trigo_type sin_theta ){
 
 abc alphabeta_to_abc(alphabeta alphabeta){    
     abc abc;
+//    alphabeta.alpha = 10 ; 
+//    alphabeta.beta = 10 ;
     
-    abc.a = SQRT_TWO_BY_TRHEE*alphabeta.alpha;
-    abc.b = abc.a - ONE_BY_SQRT_SIX*alphabeta.alpha+ONE_BY_SQRT_TWO*alphabeta.beta;
-    abc.c = abc.b - ONE_BY_SQRT_TWO*alphabeta.beta;
-    abc.a/=1000 ; 
-    abc.b/=1000 ; 
-    abc.c/=1000 ; 
+    abc.b = alphabeta.alpha*ONE_BY_SQRT_SIX;
+    abc.c = abc.b - alphabeta.beta*ONE_BY_SQRT_TWO;
+    abc.b +=  alphabeta.beta*ONE_BY_SQRT_TWO; 
+    abc.a = alphabeta.alpha*SQRT_TWO_BY_TRHEE ; 
+    
+//    abc.a/=10 ; 
+//    abc.b/=10 ; 
+//    abc.c/=10 ; 
     
     return abc;
 }
