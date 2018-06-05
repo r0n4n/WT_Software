671a9093f5fe094b6ec0fac1ef38274f914748b0
r0n4n <ronan.blanchard35@gmail.com>
2018-06-05 16:43:08 +0200

    The functions runs almost properly without over load. Some errors on the ADC could appears sometimes. The time to run the controller function is to long and must be reduced

--- transform.c
+++ transform.c
@@ -4,11 +4,11 @@
 
 #define ONE_BY_THREE 333 // 1/3 *1000  
 #define ONE_BY_SQRT_THREE 577 // 1/srqt(3) * 1000
-#define ONE_BY_SQRT_SIX 40 // 1/sqrt(6) = 0.4082
-#define ONE_BY_SQRT_TWO 70 // 1/sqrt(2)=  0.7071
+#define ONE_BY_SQRT_SIX 41 // 1/sqrt(6) = 0.4082
+#define ONE_BY_SQRT_TWO 71 // 1/sqrt(2)=  0.7071
 #define SQRT_TWO_BY_TRHEE 81 // sqrt(2/3) = 0.8165 
-#define SQRT_SIX 24  // 2.4495
-#define SQRT_TWO 14 // 1.4142
+#define SQRT_SIX 24  // sqrt(6) = 2.4495 * 10 
+#define SQRT_TWO 14 // sqrt(2) = 1.4142 * 10 
 #define ONE_BY_SQRT_TWO_BY_TRHEE 1224 //1.2247
  
 
@@ -19,27 +19,45 @@
  * Take 40 µs to perform the tranformation with long int type 
  * Take 3.2 µs to perform the transformation with fractional type 
  */
+/*
+*   Gain = x10  
+*/
 alphabeta abc_to_alphabeta(abc abc) {
     alphabeta alphabeta;
     
-    alphabeta.alpha = (2*abc.a - abc.b - abc.c)/SQRT_SIX ;
+    alphabeta.alpha = (2*abc.a - abc.b - abc.c)/SQRT_SIX ; // divide by 10 
     alphabeta.beta = (abc.b - abc.c)/SQRT_TWO;
+//    alphabeta.alpha = (2*abc.a - abc.b - abc.c)*ONE_BY_SQRT_SIX ;
+//    alphabeta.beta = (abc.b - abc.c)*ONE_BY_SQRT_TWO;
 //    alphabeta.alpha /= 1000 ;
 //    alphabeta.beta /=1000 ;
+    alphabeta.alpha *= 100 ;
+    alphabeta.beta *=100 ;
     return alphabeta; // THE VALUES ARE *1 000 
 }
 
 /*Takes 60 µs */
+/*
+*   Gain = x0.1  
+*/
 dq alphabeta_to_dq(alphabeta alphabeta, float theta){
     dq dq;
 //    printf("theta=%f   \n\r", theta); 
 //    printf("theta=%f   \n\r", theta); 
-    int cos_theta = (int)(cosf(theta)*1000) ; 
-    int sin_theta = (int)(sinf(theta)*1000) ; 
+//    int cos_theta = (int)(cosf(theta)*1000) ; 
+//    int sin_theta = (int)(sinf(theta)*1000) ; 
+    int cos_theta = (int)(1/cosf(theta)*10) ; 
+    int sin_theta = (int)(1/sinf(theta)*10) ; 
+//    float cos_theta = cosf(theta) ; 
+//    float sin_theta = sinf(theta) ;
 //    printf("cos theta=%ld   ", cos_theta);
 //    printf("sin_theta=%ld   \n\r", sin_theta);
-    dq.d = cos_theta*alphabeta.alpha + sin_theta*alphabeta.beta;
-    dq.q = cos_theta*alphabeta.beta - sin_theta*alphabeta.alpha;
+    dq.d = alphabeta.alpha/cos_theta + alphabeta.beta/sin_theta;
+    dq.q = alphabeta.beta/cos_theta - alphabeta.alpha/sin_theta;
+//    dq.d = cos_theta*alphabeta.alpha + sin_theta*alphabeta.beta;
+//    dq.q = cos_theta*alphabeta.beta - sin_theta*alphabeta.alpha;
+//    dq.d = cos_theta*(float)alphabeta.alpha + sin_theta*(float)alphabeta.beta;
+//    dq.q = cos_theta*(float)alphabeta.beta - sin_theta*(float)alphabeta.alpha;
 //    dq.d/=100 ; 
 //    dq.q/=100 ; 
 //    printf("d=%ld   ",dq.d );
@@ -62,16 +80,24 @@ dq alphabeta_to_dq2(alphabeta alphabeta,  trigo_type cos_theta , trigo_type sin_
 }
 
 
-
+/* in = 
+ * Gain = x0.1
+ * out = 
+ */
 alphabeta dq_to_alphabeta(dq dq, float theta){
     alphabeta alphabeta;
-    dq.d /= 100 ; 
-    dq.q /= 100 ;
-    int cos_theta = (int)(cosf(theta)*100) ; 
-    int sin_theta = (int)(sinf(theta)*100) ; 
+//    dq.d /= 100 ; 
+//    dq.q /= 100 ;
+    int cos_theta = (int)(1/cosf(theta)*10) ; 
+    int sin_theta = (int)(1/sinf(theta)*10) ; 
     
-    alphabeta.alpha = cos_theta*dq.d - sin_theta*dq.q;
-    alphabeta.beta = sin_theta*dq.d + cos_theta*dq.q;
+//    alphabeta.alpha = cos_theta*dq.d - sin_theta*dq.q;
+//    alphabeta.beta = sin_theta*dq.d + cos_theta*dq.q;
+    alphabeta.alpha = dq.d/cos_theta - dq.q/sin_theta;
+    alphabeta.beta = dq.d/sin_theta + dq.q/cos_theta;
+    
+    alphabeta.alpha /=10 ; 
+    alphabeta.beta /= 10 ; 
     return alphabeta;
 }
 
@@ -83,6 +109,9 @@ alphabeta dq_to_alphabeta2(dq dq, trigo_type cos_theta , trigo_type sin_theta ){
     return alphabeta;
 }
 
+/*
+ * Gain = x100
+ */
 abc alphabeta_to_abc(alphabeta alphabeta){    
     abc abc;
 //    alphabeta.alpha = 10 ; 
