aa027c055bc3a69b87a29ed5c2fc594d45993990
r0n4n <ronan.blanchard35@gmail.com>
2018-06-07 09:25:58 +0200

    remove the useless comments and optimise the dq function (without sin and cos inside)

--- transform.c
+++ transform.c
@@ -27,12 +27,9 @@ alphabeta abc_to_alphabeta(abc abc) {
     
     alphabeta.alpha = (2*abc.a - abc.b - abc.c)/SQRT_SIX ; // divide by 10 
     alphabeta.beta = (abc.b - abc.c)/SQRT_TWO;
-//    alphabeta.alpha = (2*abc.a - abc.b - abc.c)*ONE_BY_SQRT_SIX ;
-//    alphabeta.beta = (abc.b - abc.c)*ONE_BY_SQRT_TWO;
-//    alphabeta.alpha /= 1000 ;
-//    alphabeta.beta /=1000 ;
     alphabeta.alpha *= 100 ;
     alphabeta.beta *=100 ;
+    
     return alphabeta; // THE VALUES ARE *1 000 
 }
 
@@ -40,42 +37,12 @@ alphabeta abc_to_alphabeta(abc abc) {
 /*
 *   Gain = x0.1  
 */
-dq alphabeta_to_dq(alphabeta alphabeta, float theta){
+dq alphabeta_to_dq(alphabeta alphabeta, trigo_type cos_theta , trigo_type sin_theta ){
     dq dq;
-//    printf("theta=%f   \n\r", theta); 
-//    printf("theta=%f   \n\r", theta); 
-//    int cos_theta = (int)(cosf(theta)*1000) ; 
-//    int sin_theta = (int)(sinf(theta)*1000) ; 
-    int cos_theta = (int)(1/cosf(theta)*10) ; 
-    int sin_theta = (int)(1/sinf(theta)*10) ; 
-//    float cos_theta = cosf(theta) ; 
-//    float sin_theta = sinf(theta) ;
-//    printf("cos theta=%ld   ", cos_theta);
-//    printf("sin_theta=%ld   \n\r", sin_theta);
+    
     dq.d = alphabeta.alpha/cos_theta + alphabeta.beta/sin_theta;
     dq.q = alphabeta.beta/cos_theta - alphabeta.alpha/sin_theta;
-//    dq.d = cos_theta*alphabeta.alpha + sin_theta*alphabeta.beta;
-//    dq.q = cos_theta*alphabeta.beta - sin_theta*alphabeta.alpha;
-//    dq.d = cos_theta*(float)alphabeta.alpha + sin_theta*(float)alphabeta.beta;
-//    dq.q = cos_theta*(float)alphabeta.beta - sin_theta*(float)alphabeta.alpha;
-//    dq.d/=100 ; 
-//    dq.q/=100 ; 
-//    printf("d=%ld   ",dq.d );
-//    printf("q=%ld   \n\r",dq.q );
-    return dq;
-}
-
-    /* Takes 3.8 µs */ 
-dq alphabeta_to_dq2(alphabeta alphabeta,  trigo_type cos_theta , trigo_type sin_theta ){
-    dq dq;
-//    printf("cos theta=%ld   ", cos_theta);
-//    printf("sin_theta=%ld   \n\r", sin_theta);
-    dq.d = cos_theta*alphabeta.alpha + sin_theta*alphabeta.beta;
-    dq.q = cos_theta*alphabeta.beta - sin_theta*alphabeta.alpha;
-//    dq.d/=1000 ; 
-//    dq.q/=1000 ; 
-//    printf("d=%ld   ",dq.d );
-//    printf("q=%ld   \n\r",dq.q );
+    
     return dq;
 }
 
@@ -84,15 +51,10 @@ dq alphabeta_to_dq2(alphabeta alphabeta,  trigo_type cos_theta , trigo_type sin_
  * Gain = x0.1
  * out = 
  */
-alphabeta dq_to_alphabeta(dq dq, float theta){
+alphabeta dq_to_alphabeta(dq dq, trigo_type cos_theta , trigo_type sin_theta){
     alphabeta alphabeta;
 //    dq.d /= 100 ; 
 //    dq.q /= 100 ;
-    int cos_theta = (int)(1/cosf(theta)*10) ; 
-    int sin_theta = (int)(1/sinf(theta)*10) ; 
-    
-//    alphabeta.alpha = cos_theta*dq.d - sin_theta*dq.q;
-//    alphabeta.beta = sin_theta*dq.d + cos_theta*dq.q;
     alphabeta.alpha = dq.d/cos_theta - dq.q/sin_theta;
     alphabeta.beta = dq.d/sin_theta + dq.q/cos_theta;
     
@@ -101,34 +63,18 @@ alphabeta dq_to_alphabeta(dq dq, float theta){
     return alphabeta;
 }
 
-alphabeta dq_to_alphabeta2(dq dq, trigo_type cos_theta , trigo_type sin_theta ){
-    alphabeta alphabeta;
-     
-    alphabeta.alpha = cos_theta*dq.d - sin_theta*dq.q;
-    alphabeta.beta = sin_theta*dq.d + cos_theta*dq.q;
-    return alphabeta;
-}
 
 /*
  * Gain = x100
  */
 abc alphabeta_to_abc(alphabeta alphabeta){    
     abc abc;
-//    alphabeta.alpha *= 10 ; 
-//    alphabeta.beta *= 10 ;
+    alphabeta.alpha *= 100 ; 
+    alphabeta.beta *= 100 ;
     
     abc.b = -alphabeta.alpha/SQRT_SIX;
     abc.c = abc.b - alphabeta.beta/SQRT_TWO;
     abc.b +=  alphabeta.beta/SQRT_TWO; 
-    abc.a = alphabeta.alpha/ONE_BY_SQRT_TWO_BY_TRHEE ; 
-    
-    //abc.a =  alphabeta.alpha/ONE_BY_SQRT_TWO_BY_TRHEE + alphabeta.beta/SQRT_TWO;
-    
-     
-    
-//    abc.a/=10 ; 
-//    abc.b/=10 ; 
-//    abc.c/=10 ; 
-    
+    abc.a = alphabeta.alpha/ONE_BY_SQRT_TWO_BY_TRHEE ;  
     return abc;
 }
