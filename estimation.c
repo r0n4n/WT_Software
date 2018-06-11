99e68650d9483adb6d987b55253a5a5515304ceb
r0n4n <ronan.blanchard35@gmail.com>
2018-06-11 18:21:42 +0200

    estimates theta with fixed point function

--- estimation.c
+++ estimation.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include "serialData.h"
 
+fractional Atan2CORDIC( fractional y, fractional x );
+
+#define THETA_STEP (INT_MAX-10) // 3*100
+#define PI_10 31 
+#define OMEGA_GAIN (INT_MAX/PI_10) 
+
 alphabeta line_voltage_estimator(abc iabc, alphabeta us_alpha_beta){
     alphabeta i_alpha_beta ; 
     alphabeta ul_alpha_beta ; 
@@ -29,33 +35,22 @@ alphabeta converter_voltage_estimator(float udc, abc S){
 
 /* Takes 8 µs to perform the estimation 
  */
-float theta_estimator(alphabeta ul){
-//    printf("ualp=%ld", ul.alpha);
-//    printf("ubet=%ld", ul.beta);
-//    printf("ualp=%lf", (double)ul.alpha);
-//    printf("ubet=%lf   \n\r", (double)ul.beta);
-//    errno = 0; 
-//    ul.alpha = 13456 ; 
-//    ul.beta = 7891011 ; s
-    
-    float beta= (float)ul.beta ; 
-    float alpha = (float)ul.alpha ; 
-//    sendData(beta) ; 
-//    sendData(alpha) ; 
-    float theta= atan2f(beta,alpha) ; // check the parameters order
-//    printf("sizeof long int =%d   \n\r", sizeof(long int) ); 
-//    printf("sizeof double=%d   \n\r", sizeof(float));
-//    printf("beta=%ld   \n\r", ul.beta); 
-//    printf("alpha=%ld   \n\r", ul.alpha); 
-//    printf("betadouble=%f   \n\r", beta); 
-//    printf("alphadouble=%f  \n\r", alpha);
-    //__delay32(600) ; // wait for the end of the conversion 
-    //printf("theta=%f   \n\r", theta); 
-//    printf("ERRNO=%d   \n\n\r", errno); 
-    return theta ; 
+int theta_estimator(alphabeta ul){
+    return Atan2CORDIC(ul.beta, ul.alpha );
 }
 
-float derivate(float old, float new, float step_time){
-    float derivate = (new-old)/step_time ; 
-    return derivate ; 
+
+
+int omega_estimation(int last, int new ){
+    static int last_omega = 0  ; 
+    if (last-new >THETA_STEP) {
+        return last_omega ;         
+    }
+    else {
+//        last /= OMEGA_GAIN ; 
+//        new /= OMEGA_GAIN ; 
+        last_omega = derivate(last, new, FS) ;
+        return last_omega ; 
+    }  
+    return -1 ;
 }
