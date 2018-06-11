b19b5f6b900cf1948ae29d048d1240161df424ee
r0n4n <ronan.blanchard35@gmail.com>
2018-06-11 18:20:29 +0200

    add generate theta function for test

--- test_functions.c
+++ test_functions.c
@@ -1,7 +1,18 @@
 #include "test_functions.h"
 
 
-
+#define MAX_THETA 314 // 3.14 * 100 
+#define MIN_THETA -314
 void three_phase(abc *abc){
+    
 //    static float angle ;
+}
+
+int generate_theta() {
+    static int theta = INT_MIN ; 
+    theta += 2  ; 
+    if (theta>INT_MAX){
+        theta = INT_MAX ; 
+    }
+    return theta ; 
 }
