f795d7c7ea834ea46ec0430b3b57777e42acc56e
r0n4n <ronan.blanchard35@gmail.com>
2018-06-14 16:46:21 +0200

    add multiply function

--- common.c
+++ common.c
@@ -6,6 +6,28 @@ int saturation(int min, int max, int value){
     else return value ; 
 }
 
+/**
+ * 
+ * @param before
+ * @param after
+ * @param ts
+ * @return 
+ */
 int derivate(int before, int after, int ts){
     return (after - before)*10/ts ;
+}
+
+
+/**
+ * This function return the product of a signed integer and a fractionnal  
+ * @param integer
+ * @param fract
+ * @return 
+ */
+_Q15 multi_integ_frac(int integer,  int fract){
+    _Q16 x = _Q16shl(integer,16) ;
+//    _Q16 y = (unsigned int)(fract<<1) ; 
+    _Q16 y = _Q16reciprocalQ15( fract )/2 ; 
+    _Q15 z = _Q16mpy(x,y)>>16 ; 
+    return z ; 
 }
--- common.h
+++ common.h
@@ -11,9 +11,13 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
+    
+#include <dsp.h>
+#include <libq.h>
 
 int saturation(int min, int max, int value) ; 
 int derivate(int before, int after, int ts) ; 
+_Q15 multi_integ_frac(int integer,  int fract) ;
 
 #ifdef	__cplusplus
 }
