4f1a39c5ef6061a173b521d72c9668c2c89590ac
r0n4n <ronan.blanchard35@gmail.com>
2018-06-14 16:45:27 +0200

    change the prototype to use global variables

--- serialData.h
+++ serialData.h
@@ -17,6 +17,13 @@ extern "C" {
 #include "mcc_generated_files/pin_manager.h"
 #include "typedef.h"
 #include <dsp.h>
+    #include "control.h"
+    
+    
+extern bool sending ;
+extern char ReceivedChar; 
+extern state state_vector ; 
+extern signal us ; 
     
 void sendData(float data) ; 
 void ReceiveData(float data) ; 
@@ -24,20 +31,26 @@ void serialInit() ;
 void sendVect(float *vect, int length ) ; 
 void setTransmitterMode() ; 
 void setReceiverMode() ;
+void limited_counter() ; 
+void listen_RS485() ;
+void send_if_required() ; 
 
-void send_measurements(state state) ; 
-void send_ul_abc_to_alphabeta(state state) ; 
-void send_ul_alphabeta_to_dq(state state) ;
-void send_il_abc_to_alphabeta(state state); 
-void send_il_alphabeta_to_dq(state state) ; 
-void send_us_dq_to_alphabeta(signal us);
-void send_us_alphabeta_to_abc(signal us); 
-void send_theta_cos_theta(state state, trigo_type cos_theta, trigo_type sin_theta ) ;
-void send_omega(int omega, int theta, int last_theta) ; 
-void send_id_controller(tPID id_PID) ; 
-void send_iq_controller(tPID iq_PID) ;
-void send_usq_decoupler_controller(state state,signal us, tPID iq_PID) ; 
-void send_usd_decoupler_controller(state state,signal us, tPID id_PID) ; 
+void send_measurements( ) ; //0
+void send_ul_abc_to_alphabeta( ) ; //1
+void send_omega() ;  //
+void send_ul_alphabeta_to_dq( ) ;
+void send_il_abc_to_alphabeta( ); 
+void send_il_alphabeta_to_dq( ) ; 
+
+void send_id_controller( ) ; 
+void send_iq_controller( ) ;
+void send_usq_decoupler_controller(  ) ; 
+void send_usd_decoupler_controller( ) ; 
+
+void send_us_dq_to_alphabeta( );
+void send_us_alphabeta_to_abc( ); 
+//void send_theta_cos_theta( state, trigo_type cos_theta, trigo_type sin_theta ) ;
+
 
 #ifdef	__cplusplus
 }
